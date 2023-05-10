6f297389c9eb7c4d2ffa7e1f4ca721ff0e578035
dg1223 <alavi1223@hotmail.com>
2023-05-10 19:36:55 -0400

    create and update DB

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,22 @@ function App() {
     const email = form.email.value;
     const user = { name, email };
     console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("Users added successfully");
+          form.reset();
+        }
+      });
   };
 
   return (
