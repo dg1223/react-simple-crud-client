b836b5d828a0139fcfefc35b0aec502fe0725393
dg1223 <alavi1223@hotmail.com>
2023-05-10 19:55:51 -0400

    delete operation

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,6 +2,21 @@ import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const users = useLoaderData();
+
+  const handleDeleteUser = (_id) => {
+    console.log("Delete: ", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("Deleted successfully");
+        }
+      });
+  };
+
   return (
     <div>
       <h2>Number of users: {users.length}</h2>
@@ -9,6 +24,7 @@ const Users = () => {
         {users.map((user) => (
           <p key={user._id}>
             {user.name}: {user.email}
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
           </p>
         ))}
       </div>
