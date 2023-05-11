471d78ce772f68d29c6009cfe27284a019d5ccf1
dg1223 <alavi1223@hotmail.com>
2023-05-11 01:24:21 -0400

    CRUD operation complete

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -9,6 +9,23 @@ const Update = () => {
     const name = form.name.value;
     const email = form.email.value;
     console.log(name, email);
+    const updatedUser = { name, email };
+
+    // send to server
+    fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          alert("user updated successfully");
+        }
+      });
   };
 
   return (
